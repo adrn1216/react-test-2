1dafcc3c7936fc139cb81c9b71939f93e8ef0a1d
adrn1216 <louisadrianreyes@yahoo.com>
2022-06-01 11:31:44 +0800

    add input for new task

--- src/pages/TaskList.jsx
+++ src/pages/TaskList.jsx
@@ -14,10 +14,24 @@ const TaskList = () => {
       done: false,
     },
   ]);
+  const [todo, setTodo] = useState("");
+
+  const addTodo = (text) => {
+    let newTask = { text: text, done: false };
+    setTasks([...tasks, newTask]);
+  };
 
   return (
     <StyledPage>
       <TodoList items={tasks} />
+      <div>
+        <input
+          type="text"
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+        />
+        <button onClick={() => addTodo(todo)}>Add</button>
+      </div>
     </StyledPage>
   );
 };
